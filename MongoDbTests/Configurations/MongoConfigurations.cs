427069f7c7957f4b40d24392bc1c9c5744c7cf9b
Tiago Santos <tiagosantos@outlook.com>
2018-06-12 13:29:02 -0300

    ok

---
+++ MongoDbTests/Configurations/MongoConfigurations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoDbTests.Configurations
+{
+    public static class MongoConfigurations
+    {
+        public const string ConnectionString = "mongodb://dotnetcore-mongo.db/?safe=true&uuidRepresentation=Standard";
+        public const string Database = "dotnetcoreTestsDb";
+    }
+}
---
+++ MongoDbTests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MongoDbTests.Configurations;
+
+namespace MongoDbTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
