8679fa64841f84e93bda33cc145ac8ea67678ef0
Tiago Santos <tiagosantos@outlook.com>
2018-07-22 12:44:19 -0300

    ok

---
+++ source/NetCoreMongo.Web/Domain/Users/Models/UserParameters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq.Expressions;
+using AspNetCoreMongoDb.Web.Domain.Users;
+using NetCoreMongo.Web.Utils;
+
+namespace NetCoreMongo.Web.Domain.Users.Models
+{
+    public class UserParameters
+    {
+        public string Query { get; set; }
+        public Guid? CountryId { get; set; }
+        public Guid? ProfessionId { get; set; }
+
+        public Expression<Func<User, bool>> ToExpression()
+        {
+            var predicate = PredicateBuilder.True<User>();
+
+            //if (!string.IsNullOrWhiteSpace(Query))
+            //{
+            //    predicate = predicate.And(p =>
+            //        p.Identification.Contains(Query) ||
+            //        p.Email.Contains(Query) ||
+            //        p.Name.ToLower().Contains(Query));
+            //}
+
+            //if (EmpresaId.HasValue)
+            //{
+            //    predicate = predicate.And(p => p.EmpresaId == EmpresaId.Value);
+            //}
+
+            return predicate;
+        }
+    }
+}
