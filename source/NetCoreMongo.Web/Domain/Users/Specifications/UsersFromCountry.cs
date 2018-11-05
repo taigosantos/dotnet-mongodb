e3ed3c8c71e70a6770833a354f034afb97cfe62f
Tiago Santos <taigobrasil@gmail.com>
2018-11-05 17:06:48 -0200

    ok

---
+++ source/NetCoreMongo.Web/Domain/Users/Specifications/UsersFromCountry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using LinqSpecs;
+
+namespace NetCoreMongo.Web.Domain.Users.Specifications
+{
+    public class UsersFromCountry : Specification<User>
+    {
+        private string _countryId;
+
+        public UsersFromCountry(string countryId)
+        {
+            _countryId = countryId;
+        }
+
+        public override Expression<Func<User, bool>> ToExpression()
+        {
+            return user => user.CountryId == _countryId;
+        }
+    }
+}
